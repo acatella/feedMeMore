65714f915d68857aaea77cf6095f6413c510fbdd
acatella <aficclark@gmail.com>
2016-08-04 18:57:03 -0700

    wrote html for admin page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,10 +26,10 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       })
       .otherwise({
         redirectTo: '/'
