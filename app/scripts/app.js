b5192e704d0e8b4e042dca3433660b2cb0c2d11b
acatella <aficclark@gmail.com>
2016-08-22 20:57:32 -0700

    re-linked all nav items

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,6 +36,71 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/club-history', {
+        templateUrl: 'views/club-history.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/club-rules', {
+        templateUrl: 'views/club-rules.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/board', {
+        templateUrl: 'views/board.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/staff', {
+        templateUrl: 'views/staff.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/swim-team', {
+        templateUrl: 'views/swim-team.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/adult-swim', {
+        templateUrl: 'views/adult-swim.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/waterpolo', {
+        templateUrl: 'views/waterpolo.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/swim-lessons', {
+        templateUrl: 'views/swim-lessons.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/tennis-team', {
+        templateUrl: 'views/tennis-team.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/adult-tennis', {
+        templateUrl: 'views/adult-tennis.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/tennis-lessons', {
+        templateUrl: 'views/tennis-lessons.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/calendar', {
+        templateUrl: 'views/calendar.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/events', {
+        templateUrl: 'views/events.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
       .otherwise({
         redirectTo: '/'
       });
